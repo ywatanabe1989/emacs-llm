c98e1c233513c413b360ca545fb2f0864c527df0
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-02-27 09:30:57 +1100

    update

--- emacs-llm-scroll.el
+++ emacs-llm-scroll.el
@@ -1,62 +1,7 @@
 ;;; -*- coding: utf-8; lexical-binding: t -*-
 ;;; Author: ywatanabe
-;;; Timestamp: <2025-02-26 21:31:21>
+;;; Timestamp: <2025-02-27 09:30:38>
 ;;; File: /home/ywatanabe/.dotfiles/.emacs.d/lisp/emacs-llm/emacs-llm-scroll.el
-
-(defun --el-scroll-to-bottom
-    (&optional buffer-name)
-  "Scrolls to the end of the LLM buffer.
-Optional BUFFER-NAME can be provided, defaults to \"*Emacs-LLM*\"."
-  (interactive)
-  (let*
-      ((buffer-name
-        (or buffer-name --el-buffer-name))
-       (buffer
-        (get-buffer buffer-name)))
-    (when buffer
-      (with-current-buffer buffer
-        (goto-char
-         (point-max))
-        (let
-            ((window
-              (get-buffer-window buffer 0)))
-          (when window
-            (with-selected-window window
-              (goto-char
-               (point-max))
-              (recenter -1))))))))
-
-(defun --el-scroll-to-point
-    (position)
-  "Scroll to POSITION in LLM buffer."
-  (let
-      ((buffer
-        (get-buffer-create --el-buffer-name)))
-    (with-current-buffer buffer
-      (goto-char position)
-      (let
-          ((window
-            (get-buffer-window buffer 0)))
-        (when window
-          (with-selected-window window
-            (recenter)))))))
-
-(defun --el-scroll-to-bottom
-    ()
-  "Scroll to the end of the LLM buffer."
-  (interactive)
-  (let
-      ((buffer
-        (get-buffer-create --el-buffer-name)))
-    (with-current-buffer buffer
-      (goto-char
-       (point-max))
-      (let
-          ((window
-            (get-buffer-window buffer 0)))
-        (when window
-          (with-selected-window window
-            (recenter -1)))))))
 
 (defun --el-scroll-to-last-separator
     ()
